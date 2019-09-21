3d8567a66c0c322dc345782c06a39e3e554e8728
beerfect <beerfect@yandex.ru>
2019-09-21 18:02:55 +0300

    adaptive for hdpi devices

--- 03 canvas in text/js/script.js
+++ 03 canvas in text/js/script.js
@@ -28,16 +28,17 @@ function main() {
 	scene.add(cube);
 
 	// adaptive render size
-	function resizeRendererToDisplaySize(renderer) {
+    function resizeRendererToDisplaySize(renderer) {
 		const canvas = renderer.domElement;
-		const width = canvas.clientWidth;
-		const height = canvas.clientHeight;
+		const pixelRatio = window.devicePixelRatio;
+		const width = canvas.clientWidth * pixelRatio | 0;
+		const height = canvas.clientHeight * pixelRatio | 0;
 		const needResize = canvas.width !== width || canvas.height !== height;
 		if (needResize) {
-			renderer.setSize(width, height, false);
+		  renderer.setSize(width, height, false);
 		}
 		return needResize;
-	}
+	  }
 
 	// render
 	function render(time) {
